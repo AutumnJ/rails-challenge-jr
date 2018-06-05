f5265dcaa31982f957ad09d63c023f14f3301cae
Autumn Kangas <autumnkangas@gmail.com>
2018-06-05 14:22:32 -0500

    cleaned up routes and removed unused routes

--- tmwsd/config/routes.rb
+++ tmwsd/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'messages#new', as: 'root'
+
+  resources :messages, only: [:new, :create, :show, :index], param: :token
+
+  get '/messages/:token/authenticate', to: 'messages#authenticate', as: 'authenticate_message'
+  post '/messages/:token/authenticate', to: 'messages#verify_password', as: 'verify_password'
+
 end

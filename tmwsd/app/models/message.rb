90dd8f2b4af019f4349a36a71f978d343b80ab26
Autumn Kangas <autumnkangas@gmail.com>
2018-06-15 11:38:12 -0500

    set token before save on message model

--- tmwsd/app/models/message.rb
+++ tmwsd/app/models/message.rb
@@ -2,9 +2,20 @@ class Message < ApplicationRecord
 
   has_secure_password :validations => false
 
+
   validates :body, presence: true
   # Email functionality not currently in use
   # validates :email, presence: true
-  validates :token, presence: true
+
+  # With the token validation, set_token method doesn't have a chance to run
+  # validates :token, presence: true
+
+  before_save :set_token
+
+
+  def set_token
+    #switch from hex for better security
+    self.token = SecureRandom.uuid
+  end
   
 end

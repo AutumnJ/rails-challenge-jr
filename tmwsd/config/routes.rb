b1dbdf8c37bbe267592460c1a58ec4bf86407f32
Autumn Kangas <autumnkangas@gmail.com>
2018-06-15 11:37:46 -0500

    remove unused messages/index route

--- tmwsd/config/routes.rb
+++ tmwsd/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: 'messages#new', as: 'root'
 
-  resources :messages, only: [:new, :create, :show, :index], param: :token
+  resources :messages, only: [:new, :create, :show], param: :token
 
   get '/messages/:token/authenticate', to: 'messages#authenticate', as: 'authenticate_message'
   post '/messages/:token/authenticate', to: 'messages#verify_password', as: 'verify_password'
